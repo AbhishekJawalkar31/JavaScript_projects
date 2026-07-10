e2f6bd1bf0a5b27fa84d8e3597e66687ae5ddcd9
Abhishek Jawalkar <jawalkarabhishek57@gmail.com>
2026-07-10 17:42:12 +0530

    New Project

---
+++ 05-Unlimited-Colors/script.js
@@ -0,0 +1,32 @@
+const start = document.querySelector("#start");
+const stop = document.querySelector("#stop");
+
+const randomColor = function() {
+    const hex = "0123456789ABCDEF";
+
+    let color = "#";
+    for(let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
+};
+
+let intervalId;
+function startChangingColor() {
+    if(!intervalId) {
+        intervalId = setInterval(changeBgColor, 1000);
+    }
+
+    function changeBgColor() {
+        document.body.style.backgroundColor = randomColor();
+    }
+};
+
+function stopChangingColor() {
+    clearInterval(intervalId);
+    intervalId = null;
+};
+
+start.addEventListener("click", startChangingColor);
+stop.addEventListener("click", stopChangingColor);
